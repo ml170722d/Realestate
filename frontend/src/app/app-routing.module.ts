192ed18db35b884459ff19fb97177d3e80f878dd
Luka <ml170722.etf@gmail.com>
2022-02-03 18:55:03 +0100

    implement new components and routs to app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'login',
+    redirectTo: '',
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
   },
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { AdminComponent } from './components/admin/admin.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { UserWelcomeComponent } from './components/user-welcome/user-welcome.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BotDetectCaptchaModule } from 'angular-captcha';
+import { NewAgencyFormComponent } from './components/new-agency-form/new-agency-form.component';
+import { NewLocationFormComponent } from './components/new-location-form/new-location-form.component';
+import { ChangePassComponent } from './components/change-pass/change-pass.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    LogoutComponent,
+    UserCardComponent,
+    UserWelcomeComponent,
+    RegisterComponent,
+    NewAgencyFormComponent,
+    NewLocationFormComponent,
+    ChangePassComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BotDetectCaptchaModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
