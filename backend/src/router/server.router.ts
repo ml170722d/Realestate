ad194e47569811307015fde13721f50c9176c90d
Luka <ml170722.etf@gmail.com>
2022-02-06 18:26:17 +0100

    add: location controller

--- backend/src/router/server.router.ts
+++ backend/src/router/server.router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { locationRouter } from "./location.router";
 import { postRouter } from "./post.router";
 import { userRouter } from "./user.router";
 
@@ -6,5 +7,6 @@ const serverRouter = Router();
 
 serverRouter.use("/user", userRouter);
 serverRouter.use("/post", postRouter);
+serverRouter.use("/loc", locationRouter);
 
 export { serverRouter };
