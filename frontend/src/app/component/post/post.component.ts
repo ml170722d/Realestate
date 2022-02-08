e73c4ce384df3f1f1f85b0c40e0560da170c90c1
Luka <ml170722.etf@gmail.com>
2022-02-08 18:17:17 +0100

    add: single post details component

---
+++ frontend/src/app/component/post/post.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Logger from 'js-logger';
+import { PostService } from 'src/app/service/post.service';
+import Post from 'src/model/post.model';
+import Responce from 'src/model/responce.model';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss'],
+})
+export class PostComponent implements OnInit {
+  constructor(private ps: PostService, private r: Router) {}
+
+  ngOnInit(): void {
+    const tmp = this.r.url.split('/');
+    this.id = tmp[tmp.length - 1];
+
+    this.ps.get(this.id).subscribe(
+      (d) => this.onSuccess(d),
+      (e) => this.onFail(e)
+    );
+  }
+
+  id: string = '';
+  post: Post = new Post();
+
+  private onSuccess(data: Responce) {
+    this.post = data.body;
+    console.log(this.post.location);
+  }
+
+  private onFail(error: any) {
+    Logger.debug(error);
+  }
+}
