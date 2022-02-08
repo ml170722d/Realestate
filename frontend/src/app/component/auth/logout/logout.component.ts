36ba7af56e78256bc3e30fdbc088b38936c85259
Luka <ml170722.etf@gmail.com>
2022-02-08 14:59:56 +0100

    add: login & logout component

---
+++ frontend/src/app/component/auth/logout/logout.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Logger from 'js-logger';
+import { UserService } from 'src/app/service/user.service';
+import Responce from 'src/model/responce.model';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.scss'],
+})
+export class LogoutComponent implements OnInit {
+  constructor(private us: UserService, private r: Router) {}
+
+  ngOnInit(): void {}
+
+  @Input() id: string = '';
+
+  logout() {
+    this.us.logout({ id: this.id }).subscribe(
+      (d) => this.onSuccess(d),
+      (e) => this.onFail(e)
+    );
+  }
+
+  private onSuccess(data: Responce) {
+    this.r.navigate(['']);
+  }
+
+  private onFail(error: any) {
+    Logger.debug(error);
+  }
+}
