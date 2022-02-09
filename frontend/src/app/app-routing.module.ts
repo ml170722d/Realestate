6f3b0f06d9e93c343e43c35972a0d07d034e44ec
Luka <ml170722.etf@gmail.com>
2022-02-09 20:02:53 +0100

    add: implement current pages and components to app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostComponent } from './component/post/post.component';
+import { ChangeDataComponent } from './component/user/change-data/change-data.component';
+import { LoginPageComponent } from './page/login-page/login-page.component';
+import { RegisterPageComponent } from './page/register-page/register-page.component';
+import { UserPageComponent } from './page/user-page/user-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    redirectTo: '',
+  },
+  {
+    path: '',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'user',
+    component: UserPageComponent,
+  },
+  {
+    path: 'user/change/:id',
+    component: ChangeDataComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+  },
+  {
+    path: 'post/:id',
+    component: PostComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
